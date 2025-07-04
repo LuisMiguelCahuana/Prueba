fcc28e841dba88a2617792a877aa85886500f2a7
LuisMiguelCahuana <88464302+LuisMiguelCahuana@users.noreply.github.com>
2025-07-04 08:41:29 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -52,7 +52,7 @@ def convertir_fecha_hora(fecha_hora_str):
 
 # --- FORMULARIO DE LOGIN ---azul=#007BFF verde=#28a745
 if not st.session_state.logged_in:
-    st.markdown("<h3 style='text-align: center; color: #008080;'>HUMANO INGRESA TUS CREDENCIALES DE SIGOF WEB📋</h3>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center; color: #00FF00;'>HUMANO INGRESA TUS CREDENCIALES DE SIGOF WEB📋</h3>", unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
